65f6e4444f9754ffe9b1918d6f9bfdf4a473a679
CherrieDinoy <chidinoy@gmail.com>
2018-05-12 16:23:22 +0800

    Adding method.

---
+++ ConservativeSum.java
@@ -0,0 +1,11 @@
+/**
+ * Created by cd185134 on 5/12/2018.
+ */
+
+public class ConservativeSum {
+    static int countConsecutive(int N)
+    {
+        int count = 0;
+        return count;
+    }
+}
