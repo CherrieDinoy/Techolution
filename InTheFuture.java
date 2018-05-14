cabffa87a6636e2676891cdad606112089bebbc3
CherrieDinoy <chidinoy@gmail.com>
2018-05-14 11:55:33 +0800

    adding comment as per required in the constrains
    Signed-off-by: CherrieDinoy <chidinoy@gmail.com>

---
+++ InTheFuture.java
@@ -0,0 +1,20 @@
+/**
+ * Created by cd185134 on 5/14/2018.
+ */
+
+public class InTheFuture {
+
+    static int minNum(int A, int K, int P){
+        return P/(K-A)+1;
+    }
+    public static void main(String[] args) {
+        int asha = 4;
+        int kelly = 5;
+        int p = 1;
+        System.out.println(minNum(asha,kelly,p));
+        asha = 3;
+        kelly = 5;
+        p = 1;
+        System.out.println(minNum(asha,kelly,p));
+    }
+}
