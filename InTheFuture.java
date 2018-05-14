d29972fdd4fb1d67d61f864030bdbe80b025e8e7
CherrieDinoy <30861139+CherrieDinoy@users.noreply.github.com>
2018-05-14 12:53:04 +0800

    Update InTheFuture.java

--- InTheFuture.java
+++ InTheFuture.java
@@ -5,7 +5,13 @@
 public class InTheFuture {
 
     static int minNum(int A, int K, int P){
-        return P/(K-A)+1;
+         static int minNum(int A, int K, int P){
+        int min = (K-A)+1;
+        if(min != 0){
+            return P/(K-A)+1;
+        }
+        else
+            return -1;    
     }
     public static void main(String[] args) {
         int asha = 4;
